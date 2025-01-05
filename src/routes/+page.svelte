79518310ad0fee9cc3fb65c65ea5dbc2ff665339
Alena <al.gnevusheva@gmail.com>
2025-01-05 14:12:50 +0100

    clean up demo stuff & set background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,22 @@
 <script>
-	import { onMount, onDestroy } from 'svelte';
-	import { io } from 'socket.io-client';
-
-	let socket;
+	import io from 'socket.io-client';
+	import { onMount } from 'svelte';
+	const socket = io('http://localhost:8080');
 
 	onMount(() => {
-		socket = io('http://localhost:5001');
-
-		socket.on('connect', () => {
-			console.log('Connected:', socket.id);
-		});
-
-		socket.on('welcome', (message) => {
-			console.log('Server says:', message);
-		});
-
-		socket.emit('my_event', 'Hello from Svelte client!');
-
+		socket.emit('my_event', 'Hello from Svelte');
 		socket.on('my_response', (data) => {
 			console.log('Response from server:', data);
 		});
-	});
-
-	onDestroy(() => {
-		if (socket) {
-			socket.disconnect();
-		}
 	});
 </script>
 
 <main>
 	<h1>Svelte + Go Socket.IO Exp</h1>
 </main>
+
+<style>
+	h1 {
+		color: yellow;
+	}
+</style>
